90cdd71d1413ad5912ac20d219df01a27ee5d9a1
Andrew Faulkner <a.g.flknr@gmail.com>
2020-04-28 08:55:25 -0400

    varchar -> nvarchar pour support des caractères avec accents

--- Creation Script/CreateScript.sql
+++ Creation Script/CreateScript.sql
@@ -22,28 +22,28 @@ CREATE SCHEMA [p];
 GO;
 
 CREATE TABLE [u].[User] (
-  [Login]           varchar(20) PRIMARY KEY,
+  [Login]           nvarchar(20) PRIMARY KEY,
   [PasswordHash]    binary(24) NOT NULL,
   [Salt]            binary(24) NOT NULL,
   [HashIterations]  int NOT NULL,
-  [NomComplet]      varchar(100) NOT NULL,
+  [NomComplet]      nvarchar(100) NOT NULL,
   [Type]            char(5) NOT NULL
 );
 
 CREATE TABLE [c].[Responsable] (
   [ResponsableID] int identity(1,1) PRIMARY KEY,
-  [Nom]           varchar(100) NOT NULL,
-  [NumTel]        varchar(100) NOT NULL,
-  [Email]         varchar(200) NOT NULL
+  [Nom]           nvarchar(100) NOT NULL,
+  [NumTel]        nvarchar(100) NOT NULL,
+  [Email]         nvarchar(200) NOT NULL
 );
 
 CREATE TABLE [c].[Cinema] (
   [CinemaID]        int identity(1,1) PRIMARY KEY,
-  [Nom]             varchar(100) NOT NULL,
-  [Adresse]         varchar(200) NOT NULL,
+  [Nom]             nvarchar(100) NOT NULL,
+  [Adresse]         nvarchar(200) NOT NULL,
   [IsActive]        bit NOT NULL,
   [ResponsableID]   int NOT NULL,
-  [ProgrammerLogin] varchar(100) NULL,
+  [ProgrammerLogin] nvarchar(100) NULL,
   CONSTRAINT FK_Cinema_Responsable FOREIGN KEY  ([ResponsableID])
   REFERENCES [c].[Responsable] ([ResponsableID]),
   CONSTRAINT FK_Cinema_User FOREIGN KEY ([ProgrammerLogin])
@@ -52,9 +52,9 @@ CREATE TABLE [c].[Cinema] (
 
 CREATE TABLE [c].[Salle] (
   [SalleID]   int identity(1,1) PRIMARY KEY,
-  [Nom]       varchar(100) NOT NULL,
-  [TypeEcran] varchar(100) NULL,
-  [SystemSon] varchar(100) NULL,
+  [Nom]       nvarchar(100) NOT NULL,
+  [TypeEcran] nvarchar(100) NULL,
+  [SystemSon] nvarchar(100) NULL,
   [IsActive]  bit NOT NULL,
   [CinemaID]  int NOT NULL,
   CONSTRAINT  FK_Salle_Cinema FOREIGN KEY ([CinemaID])
@@ -62,8 +62,8 @@ CREATE TABLE [c].[Salle] (
 );
 
 CREATE TABLE [co].[Oeuvre] (
-  [Titre]       varchar(200) PRIMARY KEY,
-  [Description] varchar(2000) NOT NULL,
+  [Titre]       nvarchar(200) PRIMARY KEY,
+  [Description] nvarchar(2000) NOT NULL,
   [Annee]       int NOT NULL,
   [RuntimeMins] int NOT NULL,
   [Rating]      decimal(3,1),
@@ -73,20 +73,20 @@ CREATE TABLE [co].[Oeuvre] (
 );
 
 CREATE [co].[Acteur] (
-  [Nom] varchar(200) PRIMARY KEY
+  [Nom] nvarchar(200) PRIMARY KEY
 );
 
 CREATE [co].[Directeur] (
-  [Nom] varchar(200) PRIMARY KEY 
+  [Nom] nvarchar(200) PRIMARY KEY 
 );
 
 CREATE [co].[Genre] (
-  [Nom] varchar(100) PRIMARY KEY
+  [Nom] nvarchar(100) PRIMARY KEY
 );
 
 CREATE [co].[OeuvreActeur] (
-  [OeuvreTitre]   varchar(200) NOT NULL,
-  [ActeurNom]  varchar(200) NOT NULL,
+  [OeuvreTitre]   nvarchar(200) NOT NULL,
+  [ActeurNom]  nvarchar(200) NOT NULL,
   CONSTRAINT  [PK_OeuvreActeur] PRIMARY KEY ([OeuvreTitre], [ActeurNom]),
   CONSTRAINT  [FK_OeuvreActeur_Oeuvre] FOREIGN KEY ([OeuvreTitre])
   REFERENCES  [co].[Oeuvre] ([Titre]),
@@ -95,8 +95,8 @@ CREATE [co].[OeuvreActeur] (
 );
 
 CREATE [co].[OeuvreDirecteur] (
-  [OeuvreTitre]   varchar(200) NOT NULL,
-  [DirecteurNom]  varchar(200) NOT NULL,
+  [OeuvreTitre]   nvarchar(200) NOT NULL,
+  [DirecteurNom]  nvarchar(200) NOT NULL,
   CONSTRAINT  [PK_OeuvreDirecteurr] PRIMARY KEY ([OeuvreTitre], [DirecteurNom]),
   CONSTRAINT  [FK_OeuvreDirecteur_Oeuvre] FOREIGN KEY ([OeuvreTitre])
   REFERENCES  [co].[Oeuvre] ([Titre]),
@@ -105,8 +105,8 @@ CREATE [co].[OeuvreDirecteur] (
 );
 
 CREATE [co].[OeuvreGenre] (
-  [OeuvreTitre]   varchar(200) NOT NULL,
-  [GenreNom]  varchar(200) NOT NULL,
+  [OeuvreTitre]   nvarchar(200) NOT NULL,
+  [GenreNom]  nvarchar(200) NOT NULL,
   CONSTRAINT  [PK_OeuvreGenre] PRIMARY KEY ([OeuvreTitre], [GenreNom]),
   CONSTRAINT  [FK_OeuvreGenre_Oeuvre] FOREIGN KEY ([OeuvreTitre])
   REFERENCES  [co].[Oeuvre] ([Titre]),
@@ -116,7 +116,7 @@ CREATE [co].[OeuvreGenre] (
 
 CREATE TABLE [p].[Seance] (
   [SeanceID]    int         IDENTITY(1,1) PRIMARY KEY,
-  [Titre]       varchar(20) NOT NULL,
+  [Titre]       nvarchar(20) NOT NULL,
   [HeureDebut]  datetime    NOT NULL,
   [HeureFin]    datetime    NOT NULL,
   [SalleID]     int         NOT NULL,
