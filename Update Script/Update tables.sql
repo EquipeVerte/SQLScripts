659739298c76bf5eac40fab84cdaa6dff5180029
MyleneMarois <64213272+MyleneMarois@users.noreply.github.com>
2020-05-22 01:25:30 -0400

    Script pour updater la base de données

---
+++ Update Script/Update tables.sql
@@ -0,0 +1,33 @@
+/* type est un mot réservé? */
+
+ALTER TABLE [DBO].[Contenu]
+	ADD typage VARCHAR(100) NULL;
+
+
+CREATE TABLE [dbo].[ContenuPromo] (
+  [Titre]   nvarchar(200)  NOT NULL PRIMARY KEY,
+  [RuntimeMins] int NOT NULL,
+);
+
+CREATE TABLE [dbo].[SeanceContenu] (
+  [ContenuTitre]   nvarchar(200) NOT NULL,
+  [SeanceID]       int NOT NULL,
+  [indexOrdre]     int NULL,
+  [estPrincipal]   bit NULL,
+  CONSTRAINT  [PK_ContenuSeance] PRIMARY KEY ([ContenuTitre], [SeanceID]),
+  CONSTRAINT  [FK_ContenuSeance_Contenu] FOREIGN KEY ([ContenuTitre])
+  REFERENCES  [dbo].[Contenu] ([Titre]),
+  CONSTRAINT  [FK_ContenuSeance_Seance] FOREIGN KEY ([SeanceID])
+  REFERENCES  [dbo].[Seance] ([SeanceID])
+);
+
+CREATE TABLE [dbo].[SeancePromo] (
+  [PromoTitre]   nvarchar(200) NOT NULL,
+  [SeanceID]     int NOT NULL,
+  [indexOrdre]   int NULL,
+  CONSTRAINT  [PK_PromoSeance] PRIMARY KEY ([PromoTitre], [SeanceID]),
+  CONSTRAINT  [FK_PromoSeance_Promo] FOREIGN KEY ([PromoTitre])
+  REFERENCES  [dbo].[ContenuPromo] ([Titre]),
+  CONSTRAINT  [FK_PromoSeance_Seance] FOREIGN KEY ([SeanceID])
+  REFERENCES  [dbo].[Seance] ([SeanceID])
+);
