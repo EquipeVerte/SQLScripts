54f6a25436953b97cc60d78d6592a8ca99833ac2
MyleneMarois <64213272+MyleneMarois@users.noreply.github.com>
2020-05-27 14:01:56 -0400

    Separation de DROP CONSTRAINT et COLUMN

--- Update Script/Update tables.sql
+++ Update Script/Update tables.sql
@@ -26,7 +26,9 @@ INSERT INTO SeanceContenu (ContenuTitre, SeanceID, indexOrdre, estPrincipal)
 							    
 -- Supprimer la colonne qui n'est plus utilisée.
 ALTER TABLE [Seance]
-  DROP CONSTRAINT FK_Seance_Contenu
+  DROP CONSTRAINT FK_Seance_Contenu;
+
+ALTER TABLE [Seance]
   DROP COLUMN [ContenuTitre];
 
 CREATE TABLE [dbo].[SeancePromo] (
